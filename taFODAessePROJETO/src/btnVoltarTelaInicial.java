42152d9aa9c61b7734aefe9958c2814d3f396648
Rhuan Cruz <rhuanvictor883@gmail.com>
2023-11-20 12:47:39 -0300

    update RhuanCruz

--- taFODAessePROJETO/src/btnVoltarTelaInicial.java
+++
@@ -1,9 +0,0 @@
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
-public interface btnVoltarTelaInicial {
-
-    static void setOnAction(EventHandler<ActionEvent> eventHandler) {
-    }
-
-}
--- taFODAessePROJETO/src/taFODAessePROJETO/src/app/AppInicializador.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import javafx.application.Application;
-
-public class AppInicializador {
-    public static void main(String[] args) {
-        Application.launch(App.class, args);
-    }
-}
